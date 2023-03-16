8cc1c8ab1be27c9b6ec185a82b88cf6a04b7ad51
Veronica Conesa <vero.gconesa@gmail.com>
2023-03-16 22:04:26 +0100

    Checked connexion

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
+app.get('/', (req, res) => {
+  res.send('Hello Wordl!')
+})
+
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`)
 })
