ace89310b79c86bb4403ec3502805a188b3f8f26
Vero Conesa <vero.gconesa@gmail.com>
2023-03-17 21:18:28 +0100

    🐛 fix fixed class model

--- models/classes.js
+++ models/classes.js
@@ -1,33 +1,42 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 
-const ClassScheme = new Schema({
-  name: {
-    type: String
-  },
-  duration: {
-    lections: {
+const classScheme = new Schema(
+  {
+    name: {
+      type: String
+    },
+    duration: {
+      lections: {
+        type: Number
+      },
+      start: {
+        type: Date
+      },
+      end: {
+        type: Date
+      }
+    },
+    time: {
+      type: Date
+    },
+    instructor: {
+      type: String
+    },
+    level: {
+      type: String,
+      student: ['adult', 'child']
+    },
+    capacity: {
       type: Number
-    },
-    start: {
-      type: Date
-    },
-    end: {
-      type: Date
     }
-  },
-  time: {
-    type: Date
-  },
-  instructor: {
-    type: String
-  },
-  level: {
-    type: String
-  },
-  capacity: {
-    type: Number
-  }
-})
 
-module.exports = mongoose.model('classes', ClassScheme)
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+
+const Class = mongoose.model('Class', classScheme)
+module.exports = Class
