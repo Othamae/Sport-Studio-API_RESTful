3c168526c4cc71f1b520d5743dde189b9f109579
Vero Conesa <vero.gconesa@gmail.com>
2023-03-20 14:35:38 +0100

    📚 docs Added doc

--- routes/instructors.js
+++ routes/instructors.js
@@ -37,7 +37,7 @@ router.get('/instructors', getAllInstructors)
 
 /**
  * @swagger
- * /api/instructors:
+ * /api/Instructors:
  *   post:
  *     summary: Create a new instructor user
  *     description: This can only be done by the logged in user as instructor
@@ -63,10 +63,100 @@ router.get('/instructors', getAllInstructors)
  */
 router.post('/instructors', addInstructor)
 
+/**
+ * @swagger
+ * /api/instructors/{id}:
+ *  get:
+ *     summary: Find a instructor by ID
+ *     description: Returns a single instructor
+ *     tags: [Instructors]
+ *     responses:
+ *       200:
+ *         description: The detail of a instructor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#components/schemas/Users'
+ */
 router.get('/instructors/:id', getInstructor)
 
+/**
+ * @swagger
+ * /api/instructors/{id}:
+ *  put:
+ *     summary: Update an existing instructor
+ *     description: Update an existing instructor by ID
+ *     tags: [Instructors]
+ *     requestBody:
+ *        description: Update an existing instructor by ID
+ *        content:
+ *          application/json:
+ *             schema:
+ *                $ref: '#components/schemas/Users'
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: Instructor successfully updated!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#components/schemas/Users'
+ *       400:
+ *          description: Invalid ID supplied
+ *       404:
+ *          description: Instructor not found
+ *       405:
+ *          description: Validation exception
+ *
+ */
 router.put('/instructors/:id', updateInstructor)
 
+/**
+ * @swagger
+ * /api/instructors/{id}:
+ *  delete:
+ *     summary: Delete a instructor
+ *     description: Delete an existing instructor by ID
+ *     tags: [Instructors]
+ *     parameters:
+ *       - name: api_key
+ *         in: header
+ *         description: ''
+ *         required: false
+ *         schema:
+ *              type: string
+ *       - name: instructorId
+ *         in: header
+ *         description: ''
+ *         required: true
+ *         schema:
+ *              type: integer
+ *              format: int64
+ *     requestBody:
+ *        description: Update an existing instructor by ID
+ *        content:
+ *          application/json:
+ *             schema:
+ *                $ref: '#components/schemas/Users'
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: Instructor successfully deleted!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#components/schemas/Users'
+ *       400:
+ *          description: Invalid ID supplied
+ *
+ *
+ */
 router.delete('/instructors/:id', deleteInstructor)
 
 module.exports = router
--- routes/students.js
+++ routes/students.js
@@ -4,7 +4,7 @@ const { getAllStudents, addStudent, getStudent, updateStudent, deleteStudent } =
 
 /**
  * @swagger
- * /api/instructors:
+ * /api/students:
  *   get:
  *     summary: Returns a list of all instructors
  *     tags: [Students]
@@ -63,10 +63,100 @@ router.get('/students', getAllStudents)
  */
 router.post('/students', addStudent)
 
+/**
+ * @swagger
+ * /api/students/{id}:
+ *  get:
+ *     summary: Find a student by ID
+ *     description: Returns a single student
+ *     tags: [Students]
+ *     responses:
+ *       200:
+ *         description: The detail of a student
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#components/schemas/Users'
+ */
 router.get('/students/:id', getStudent)
 
+/**
+ * @swagger
+ * /api/students/{id}:
+ *  put:
+ *     summary: Update an existing student
+ *     description: Update an existing student by ID
+ *     tags: [Students]
+ *     requestBody:
+ *        description: Update an existing student by ID
+ *        content:
+ *          application/json:
+ *             schema:
+ *                $ref: '#components/schemas/Users'
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: Student successfully updated!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#components/schemas/Users'
+ *       400:
+ *          description: Invalid ID supplied
+ *       404:
+ *          description: Student not found
+ *       405:
+ *          description: Validation exception
+ *
+ */
 router.put('/students/:id', updateStudent)
 
+/**
+ * @swagger
+ * /api/students/{id}:
+ *  delete:
+ *     summary: Delete a student
+ *     description: Delete an existing student by ID
+ *     tags: [Students]
+ *     parameters:
+ *       - name: api_key
+ *         in: header
+ *         description: ''
+ *         required: false
+ *         schema:
+ *              type: string
+ *       - name: studentId
+ *         in: header
+ *         description: ''
+ *         required: true
+ *         schema:
+ *              type: integer
+ *              format: int64
+ *     requestBody:
+ *        description: Update an existing student by ID
+ *        content:
+ *          application/json:
+ *             schema:
+ *                $ref: '#components/schemas/Users'
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: Student successfully deleted!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#components/schemas/Users'
+ *       400:
+ *          description: Invalid ID supplied
+ *
+ *
+ */
 router.delete('/students/:id', deleteStudent)
 
 module.exports = router
