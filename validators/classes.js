51de94fcf69ba0e0c4d8a57cb696ac075009fb9d
Vero Conesa <vero.gconesa@gmail.com>
2023-03-21 09:56:55 +0100

    ⚡️ perf Created classes validator

---
+++ validators/classes.js
@@ -0,0 +1,47 @@
+const { check } = require('express-validator')
+const { validateResult } = require('../utils/handleValidators')
+
+const validatorCreateClass = [
+  check('name')
+    .exists()
+    .notEmpty(),
+  check('instructor')
+    .exists()
+    .notEmpty(),
+  check('level')
+    .exists()
+    .notEmpty(),
+  check('capacity')
+    .exists()
+    .notEmpty(),
+  check('time')
+    .exists()
+    .notEmpty(),
+  check('duration')
+    .exists()
+    .notEmpty(),
+  check('duration.lections')
+    .exists()
+    .notEmpty(),
+  check('duration.start')
+    .exists()
+    .notEmpty(),
+  check('duration.end')
+    .exists()
+    .notEmpty(),
+  (req, res, next) => {
+    return validateResult(req, res, next)
+  }
+]
+
+const validatorGetClass = [
+  check('id')
+    .exists()
+    .notEmpty()
+    .isMongoId(),
+  (req, res, next) => {
+    return validateResult(req, res, next)
+  }
+]
+
+module.exports = { validatorCreateClass, validatorGetClass }
