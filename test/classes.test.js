b14f88bdb1a20afe50bc6d587c17ef41bd35bf70
Vero Conesa <vero.gconesa@gmail.com>
2023-03-22 09:44:09 +0100

    🧪 test Tested delete endpoint

--- test/classes.test.js
+++ test/classes.test.js
@@ -6,83 +6,117 @@ const { initialClasses, api } = require('./helpers')
 
 beforeEach(async () => {
   await Class.deleteMany({})
-  const class1 = new Class(initialClasses[0])
-  await class1.save()
-  const class2 = new Class(initialClasses[1])
-  await class2.save()
+
+  for (const classToSave of initialClasses) {
+    const classObject = new Class(classToSave)
+    await classObject.save()
+  }
+
+//   const class1 = new Class(initialClasses[0])
+//   await class1.save()
+//   const class2 = new Class(initialClasses[1])
+//   await class2.save()
 })
 
-test('classes are returned as json', async () => {
-  await api
-    .get('/api/classes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('GET /api/classes', () => {
+  test('classes are returned as json', async () => {
+    await api
+      .get('/api/classes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are two classes', async () => {
+    const response = await api.get('/api/classes')
+    expect(response.body.data).toHaveLength(initialClasses.length)
+  })
+
+  test('the first class is Class1', async () => {
+    const response = await api.get('/api/classes')
+    expect(response.body.data[0].name).toBe('Class1')
+  })
 })
 
-test('there are two classes', async () => {
-  const response = await api.get('/api/classes')
-  expect(response.body.data).toHaveLength(initialClasses.length)
+describe('GET /api/classes/:id', () => {
+  test('the first class is Class1', async () => {
+    const response = await api.get('/api/classes')
+    expect(response.body.data[0].name).toBe('Class1')
+  })
 })
 
-test('the first class is Class1', async () => {
-  const response = await api.get('/api/classes')
-  expect(response.body.data[0].name).toBe('Class1')
-})
-
-test('a valid class can be added', async () => {
-  const newClass = {
-    name: 'Class3',
-    time: '18:00',
-    instructor: 'Instructor3',
-    level: 'adult',
-    capacity: 9,
-    duration: {
-      lections: 14,
-      start: '22/03/2023',
-      end: '07/06/2023'
+describe('POST /api/classes', () => {
+  test('a valid class can be added', async () => {
+    const newClass = {
+      name: 'Class3',
+      time: '18:00',
+      instructor: 'Instructor3',
+      level: 'adult',
+      capacity: 9,
+      duration: {
+        lections: 14,
+        start: '22/03/2023',
+        end: '07/06/2023'
+      }
     }
-  }
-  await api
-    .post('/api/classes')
-    .send(newClass)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/classes')
+      .send(newClass)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  const response = await api.get('/api/classes')
-  const names = response.body.data.map(data => data.name)
+    const response = await api.get('/api/classes')
+    const names = response.body.data.map(data => data.name)
 
-  expect(names).toContain(newClass.name)
-  expect(response.body.data).toHaveLength(initialClasses.length + 1)
+    expect(names).toContain(newClass.name)
+    expect(response.body.data).toHaveLength(initialClasses.length + 1)
+  })
+
+  test('a class without information is not added', async () => {
+    const newClass = {
+      time: '18:00',
+      instructor: 'Instructor3',
+      level: 'adult',
+      capacity: 9
+
+    }
+    await api
+      .post('/api/classes')
+      .send(newClass)
+      .expect(403)
+
+    const response = await api.get('/api/classes')
+    expect(response.body.data).toHaveLength(initialClasses.length)
+  })
 })
 
-test('a class without information is not added', async () => {
-  const newClass = {
-    time: '18:00',
-    instructor: 'Instructor3',
-    level: 'adult',
-    capacity: 9
+describe('DELETE /api/classes/:id', () => {
+  test('a class can be deleted', async () => {
+    const response = await api.get('/api/classes')
+    // const names = response.body.data.map(data => data.name)
 
-  }
-  await api
-    .post('/api/classes')
-    .send(newClass)
-    .expect(403)
+    const classToDelete = response.body.data[0]
+    await api
+      .delete(`/api/classes/${classToDelete.id}`)
+      .expect(200)
 
-  const response = await api.get('/api/classes')
-  expect(response.body.data).toHaveLength(initialClasses.length)
-})
+    const newResponse = await api.get('/api/classes')
+    const names = newResponse.body.data.map(data => data.name)
 
-test('a class can be deleted', async () => {
-  const response = await api.get('/api/classes')
-  // const names = response.body.data.map(data => data.name)
+    expect(names).not.toContain(classToDelete.name)
+    expect(newResponse.body.data).toHaveLength(initialClasses.length - 1)
+  })
 
-  const classToDelete = response.body.data[0]
-  await api
-    .delete(`/api/classes/${classToDelete.id}`)
-    .expect(200)
+  test('a class that not exist can not be deleted', async () => {
+    await api
+      .delete('/api/classes/notExistingClass')
+      .expect(403)
 
-  const newResponse = await api.get('/api/classes')
-  expect(newResponse.body.data).toHaveLength(initialClasses.length - 1)
+    const response = await api.get('/api/classes')
+    const names = response.body.data.map(data => data.name)
+
+    expect(names).not.toContain(response.name)
+    expect(response.body.data).toHaveLength(initialClasses.length)
+  })
 })
 
 afterAll(() => {
