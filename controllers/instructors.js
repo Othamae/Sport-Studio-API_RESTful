73652e81f8a793c1447d978be762ee3a280bf216
Vero Conesa <vero.gconesa@gmail.com>
2023-03-22 11:11:40 +0100

    🆕 feat Created controllers for instructors

--- controllers/instructors.js
+++ controllers/instructors.js
@@ -1,44 +1,68 @@
+const Instructor = require('../models/instructors')
+const { handleHttpError } = require('../middlewares/handleError')
+const { encrypt } = require('../middlewares/handlePassword')
+
 const getAllInstructors = async (req, res) => {
   try {
-    // const user = req.user
-    // const data = await Class.findAllData({}) // ponemos await para esperar xq es una promesa
-    res.send('GET ALL INSTRUCTORS')
+    const data = await Instructor.find({})
+    res.send({ data })
   } catch (e) {
-    console.log('ESTE ES EL ERROR')
     console.log(e)
+    handleHttpError(res, 'ERROR_GETTING_INSTRUCTORS')
   }
 }
 
 const addInstructor = async (req, res) => {
   try {
-    res.send('add new instructor')
+    const { body } = req
+    const { email, name, password } = body
+    const hash = await encrypt(password)
+
+    const instructor = new Instructor({
+      name,
+      email,
+      password: hash
+    })
+    const newInstructor = await Instructor.create(instructor)
+    res.send({ newInstructor })
   } catch (e) {
-    console.log('ESTE ES EL ERROR')
     console.log(e)
+    handleHttpError(res, 'ERROR_CREATING_INSTRUCTOR')
   }
 }
+
 const getInstructor = async (req, res) => {
   try {
-    res.send('get instructor by id')
+    const { id } = req.params
+    const data = await Instructor.findById(id)
+    res.send({ data })
   } catch (e) {
-    console.log('ESTE ES EL ERROR')
     console.log(e)
+    handleHttpError(res, 'ERROR_GETTING_INSTRUCTOR_BY_ID')
   }
 }
+
 const updateInstructor = async (req, res) => {
   try {
-    res.send('update instructor')
+    const { id } = req.params
+    const { body } = req
+    const data = await Instructor.findByIdAndUpdate(id, body, { new: true })
+    res.send({ data })
   } catch (e) {
-    console.log('ESTE ES EL ERROR')
     console.log(e)
+    handleHttpError(res, 'ERROR_UPDATING_INSTRUCTOR')
   }
 }
+
 const deleteInstructor = async (req, res) => {
   try {
-    res.send('delete instructor')
+    const { id } = req.params
+    const data = await Instructor.deleteOne({ _id: id })
+    res.send({ data })
   } catch (e) {
-    console.log('ESTE ES EL ERROR')
     console.log(e)
+    handleHttpError(res, 'ERROR_DELETING_INSTRUCTOR')
   }
 }
+
 module.exports = { getAllInstructors, addInstructor, getInstructor, updateInstructor, deleteInstructor }
