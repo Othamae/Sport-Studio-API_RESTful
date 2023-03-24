0044b55fc97eb0e9632990de8178e64a04700cdd
Vero Conesa <vero.gconesa@gmail.com>
2023-03-24 11:15:30 +0100

    ⚡️ perf Deleted POST routes in instructors/students

--- routes/instructors.js
+++ routes/instructors.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getAllInstructors, addInstructor, getInstructor, updateInstructor, deleteInstructor } = require('../controllers/instructors')
+const { getAllInstructors, getInstructor, updateInstructor, deleteInstructor } = require('../controllers/instructors')
 
 /**
  * @swagger
@@ -34,34 +34,6 @@ const { getAllInstructors, addInstructor, getInstructor, updateInstructor, delet
  *                   example: ERR500
  */
 router.get('/instructors', getAllInstructors)
-
-/**
- * @swagger
- * /api/Instructors:
- *   post:
- *     summary: Create a new instructor user
- *     description: This can only be done by the logged in user as instructor
- *     tags: [Instructors]
- *     requestBody:
- *        description: Create a new instructor
- *        content:
- *          application/json:
- *             schema:
- *                $ref: '#components/schemas/User'
- *     required: true
- *     responses:
- *       200:
- *         description: Instructor successfully created!
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/User'
- *       405:
- *          description: Invalid input!
- */
-router.post('/instructors', addInstructor)
 
 /**
  * @swagger
--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getAllStudents, addStudent, getStudent, updateStudent, deleteStudent } = require('../controllers/students')
+const { getAllStudents, getStudent, updateStudent, deleteStudent } = require('../controllers/students')
 
 /**
  * @swagger
@@ -34,34 +34,6 @@ const { getAllStudents, addStudent, getStudent, updateStudent, deleteStudent } =
  *                   example: ERR500
  */
 router.get('/students', getAllStudents)
-
-/**
- * @swagger
- * /api/students:
- *   post:
- *     summary: Create a new student user
- *     description: This can only be done by the logged in user as student
- *     tags: [Students]
- *     requestBody:
- *        description: Create a new student
- *        content:
- *          application/json:
- *             schema:
- *                $ref: '#components/schemas/User'
- *        required: true
- *     responses:
- *       200:
- *         description: Student successfully created!
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/User'
- *       405:
- *          description: Invalid input!
- */
-router.post('/students', addStudent)
 
 /**
  * @swagger
