8f89c4ce0346198387c5a12f6080b1c4df0024a7
Vero Conesa <vero.gconesa@gmail.com>
2023-03-24 11:14:49 +0100

    🐛 fix Fixed register and login user

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const { compare } = require('../middlewares/handlePassword')
-const Instructor = require('../models/instructors')
+// const Instructor = require('../models/instructors')
 const User = require('../models/users')
 const { handleHttpError } = require('../middlewares/handleError')
 const { tokenSign } = require('../middlewares/handleJWT')
@@ -9,7 +9,7 @@ const userLogin = async (req, res) => {
     const { body } = req
 
     const { email, password } = body
-    const user = await Instructor.findOne({ email })
+    const user = await User.findOne({ email })
 
     const passwordCorrect = user === null
       ? false
@@ -17,13 +17,14 @@ const userLogin = async (req, res) => {
     if (!(user && passwordCorrect)) {
       handleHttpError(res, 'INCORRECT_USER_OR_PASSWORD', 401)
     }
-
     const data = {
       user: user.name,
-      email: user.email,
-      token: await tokenSign(user)
+      email: user.email
     }
 
+    if (user.role === 'instructor') {
+      data.token = await tokenSign(user)
+    }
     res.send({ data })
   } catch (e) {
     console.log(e)
